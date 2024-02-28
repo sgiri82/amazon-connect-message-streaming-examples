03213ddfd32764ac0d3ec1227eafedbd7f4a07be
sgiri82 <sougata.giri@gmail.com>
2024-02-28 12:52:23 +0530

    Update chat-message-streaming-examples.ts

--- bin/chat-message-streaming-examples.ts
+++ bin/chat-message-streaming-examples.ts
@@ -7,7 +7,7 @@ import * as cdk from '@aws-cdk/core';
 import { ChatMessageStreamingExamplesStack } from '../lib/chat-message-streaming-examples-stack';
 
 const app = new cdk.App();
-new ChatMessageStreamingExamplesStack(app, 'EXL18558748234ChatMessageStreamingExamplesStack', {
+new ChatMessageStreamingExamplesStack(app, '+18556421845ChatMessageStreamingExamplesStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
